4bb1a5ebf48bcfb5e8a85e52385ee08d2c55d368
Yashasvi Sriram <yash.3997@gmail.com>
2020-06-02 00:58:06 -0500

    renames project to legos, moves logic of watermelon problem into a function, adds tests 	now watermelon.rs 	1. can be compiled and run locally 	2. can be submitted on codeforces as-is 	3. can have unit-tests which can be run locally

--- src/bin/watermelon.rs
+++ src/bin/watermelon.rs
@@ -2,6 +2,20 @@
 
 use std::io::{self, Read};
 
+fn can_split_into_even_parts(num: u32) -> bool {
+    // Odd => NO
+    if num % 2 == 1 {
+        return false;
+    }
+
+    // Exception
+    if num < 3 {
+        return false;
+    }
+
+    return true;
+}
+
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin()
@@ -11,21 +25,62 @@ fn main() -> io::Result<()> {
     // 1 <= kilos <= 100
     let kilos = buffer
                     .trim()
-                    .parse::<i32>()
+                    .parse::<u32>()
                     .unwrap();
 
-    // Odd => NO
-    if kilos % 2 == 1 {
+    if can_split_into_even_parts(kilos) {
+        println!("YES");
+    } else {
         println!("NO");
-        return Ok(());
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_to_nine() {
+        assert_eq!(can_split_into_even_parts( 1u32), false);
+        assert_eq!(can_split_into_even_parts( 2u32), false);
+        assert_eq!(can_split_into_even_parts( 3u32), false);
+        assert_eq!(can_split_into_even_parts( 5u32), false);
+        assert_eq!(can_split_into_even_parts( 7u32), false);
+        assert_eq!(can_split_into_even_parts( 9u32), false);
+
+        assert_eq!(can_split_into_even_parts( 4u32), true);
+        assert_eq!(can_split_into_even_parts( 6u32), true);
+        assert_eq!(can_split_into_even_parts( 8u32), true);
     }
 
-    // Exception
-    if kilos < 3 {
-        println!("NO");
-        return Ok(());
+    #[test]
+    fn ten_to_nineteen() {
+        assert_eq!(can_split_into_even_parts(11u32), false);
+        assert_eq!(can_split_into_even_parts(13u32), false);
+        assert_eq!(can_split_into_even_parts(15u32), false);
+        assert_eq!(can_split_into_even_parts(17u32), false);
+        assert_eq!(can_split_into_even_parts(19u32), false);
+
+        assert_eq!(can_split_into_even_parts(10u32), true);
+        assert_eq!(can_split_into_even_parts(12u32), true);
+        assert_eq!(can_split_into_even_parts(14u32), true);
+        assert_eq!(can_split_into_even_parts(16u32), true);
+        assert_eq!(can_split_into_even_parts(18u32), true);
     }
 
-    println!("YES");
-    return Ok(());
+    #[test]
+    fn twenty_to_twentynine() {
+        assert_eq!(can_split_into_even_parts(21u32), false);
+        assert_eq!(can_split_into_even_parts(23u32), false);
+        assert_eq!(can_split_into_even_parts(25u32), false);
+        assert_eq!(can_split_into_even_parts(27u32), false);
+        assert_eq!(can_split_into_even_parts(29u32), false);
+
+        assert_eq!(can_split_into_even_parts(20u32), true);
+        assert_eq!(can_split_into_even_parts(22u32), true);
+        assert_eq!(can_split_into_even_parts(24u32), true);
+        assert_eq!(can_split_into_even_parts(26u32), true);
+        assert_eq!(can_split_into_even_parts(28u32), true);
+    }
 }
